9892234016f39e47e1b065930e008bcecc550529
Jesus Moctezuma <40676930+Jesinsky@users.noreply.github.com>
2021-07-14 16:11:26 -0500

    Set screen limits
    Setting the screen limits for the ship and create a speed factor

--- settings.py
+++ settings.py
@@ -7,3 +7,6 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (94, 94, 94)
+        
+        # Ship Settings
+        self.ship_speed_factor = 1.5
