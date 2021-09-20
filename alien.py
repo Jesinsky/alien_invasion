e54cb1f9a1f315f6f1dd9e45ad5158227c764cae
Jesus Moctezuma <40676930+Jesinsky@users.noreply.github.com>
2021-09-20 17:47:28 -0500

    Fixing a typo mistake

--- alien.py
+++ alien.py
@@ -35,5 +35,5 @@ class Alien(Sprite):
 
     def update(self):
         """Move the alien right."""
-        self.x += (self.ai_settings.alien_speed_factor * self.ai_settings.fleet_directions)
+        self.x += (self.ai_settings.alien_speed_factor * self.ai_settings.fleet_direction)
         self.rect.x = self.x
