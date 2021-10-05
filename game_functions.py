033244a6629dd07ef730e2788de15ee1361d2741
Jesus Moctezuma <40676930+Jesinsky@users.noreply.github.com>
2021-10-05 15:23:53 -0500

    Reset the game
    Reset the game when is over

--- game_functions.py
+++ game_functions.py
@@ -30,7 +30,7 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_a:
         ship.moving_left = False
 
-def check_events(ai_settings, screen, stats, play_button, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets):
     """Respond to to keyboard and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -41,12 +41,22 @@ def check_events(ai_settings, screen, stats, play_button, ship, bullets):
             check_keyup_events(event, ship)
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            check_play_button(stats, play_button, mouse_x, mouse_y)
+            check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y)
 
-def check_play_button(stats, play_button, mouse_x, mouse_y):
+def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
     """Start a new game whem the player clicks Play."""
     if play_button.rect.collidepoint(mouse_x, mouse_y):
+        # Reset the game settings.
+        stats.reset_stats()
         stats.game_active = True
+
+        # Empty list of aliens and bullets.
+        aliens.empty()
+        bullets.empty()
+
+        # Create the new fleet an center the ship.
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
 
 def update_bullets(ai_settings, screen, ship, aliens, bullets):
     """Update position of bullets and get rid of old bullets."""
