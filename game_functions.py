1ea27802bdcea01dd0c97167d75bbfd13f3412e9
Jesus Moctezuma <40676930+Jesinsky@users.noreply.github.com>
2021-10-06 12:22:55 -0500

    Hiding cursor
    Hiding the cursor when game is active and make it visible when the game stops

--- game_functions.py
+++ game_functions.py
@@ -46,6 +46,8 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
 def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+        # Hide the mouse cursor.
+        pygame.mouse.set_visible(False)
         """Start a new game whem the player clicks Play."""
         # Reset the game settings.
         stats.reset_stats()
@@ -167,6 +169,7 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
 
     else:
         stats.game_active = False
+        pygame.mouse.set_visible(True)
 
 def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
     """Check if any aliens have reached the bottom of the screen."""
